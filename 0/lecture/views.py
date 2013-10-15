c771dfb5172993855261189553fa7839a14cf680
gowithqi <gowithqi@126.com>
2013-10-15 21:10:52 +0800

    add 404.html and 500.html

--- 0/lecture/views.py
+++ 0/lecture/views.py
@@ -5,10 +5,18 @@ from django.shortcuts import render, get_object_or_404
 # from django.core.urlresolvers import reverse
 # from django.db.models import Avg
 
-from lecture.models import *
+from lecture.models import Course, Lecture, LectureComment, LectureCommentSuperRecord, LectureLevelRecord
+from login.models import User
 
 def lecture(request):
 	print "from hello"
 	return HttpResponse("Hello! this is lecture")
+
+def commentLecture(request):
+	# get a user
+	# get a lecture
+	# get a comment
+	lecture_comment = LectureComment.objects.create()
+
 
 
