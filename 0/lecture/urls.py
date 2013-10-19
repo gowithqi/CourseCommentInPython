f39fd759aa2b9c2e341586ba449eacbeea762cae
gowithqi <gowithqi@126.com>
2013-10-19 12:55:35 +0800

    加入了记录课程评分和学生得分的响应程序

--- 0/lecture/urls.py
+++ 0/lecture/urls.py
@@ -4,4 +4,6 @@ from lecture import views
 
 urlpatterns = patterns('',
 	url(r'^(?P<lecture_id>)\d+/$', views.getLecture, name='lecture'),
+	url(r'^recordscore/(?P<lecture_id>)\d+/$', view.recordStudentScore),
+	url(r'^recordlevel/(?P<lecture_id>)\d+/$', view.recordLevel),
 )
--- 0/lecture/views.py
+++ 0/lecture/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 # from django.core.urlresolvers import reverse
 # from django.db.models import Avg
 
-from lecture.models import Course, Lecture, LectureComment, LectureCommentSuperRecord, LectureLevelRecord
+from lecture.models import Course, Lecture, LectureComment, LectureCommentSuperRecord, LectureLevelRecord, LectureStudentScoreRecord
 from login.models import User
 from login.views import checkUserLogin
 
@@ -27,7 +27,43 @@ def getLecture(request, lecture_id):
 		'l' : lectures,
 		})
 	return HttpResponse(template.render(context))
-	
+
+def recordStudentScore(request, lecture_id):
+	if request.method != "POST": raise Http404
+
+	checkUserLogin(request)
+	user = get_object_or_404(User, id=request.session['user_id'])
+	lecture_id = int(lecture_id)
+	lecture = get_object_or_404(Lecture, id=lecture_id)
+	lss = lecture.student_score         
+	lssn = lecture.student_score_number
+	newscore = float(lss*lssn + float(request.POST['score'])) / (lssn+1)
+	lecture.student_score_number = lssn+1
+	lecture.student_score = newscore
+	lecture.save()
+
+	LectureStudentScoreRecord.objects.create(lecture=lecture, user=user, score=float(request.POST['score']))
+
+	return HttpResponse("yes")
+
+def recordLevel(request, lecture_id):
+	if request.method != "POST": raise Http404
+
+	checkUserLogin(request)
+	user = get_object_or_404(User, id=request.session['user_id'])
+	lecture_id = int(lecture_id)
+	lecture = get_object_or_404(Lecture, id=lecture_id)
+	ll = lecture.level         
+	lln = lecture.level_number
+	newlevel = float(ll*lln + float(request.POST['level'])) / (lln+1)
+	lecture.level_number = lln+1
+	lecture.level = newlevel
+	lecture.save()
+
+	LectureLevelRecord.objects.create(lecture=lecture, user=user, level=int(request.POST['level']))
+
+	return HttpResponse("yes")	
+
 def commentLecture(request):
 	# get a user
 	# get a lecture
