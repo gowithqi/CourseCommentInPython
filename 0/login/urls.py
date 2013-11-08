2d0aa765d7537a1cb85ce3b315af75028196664d
gowithqi <gowithqi@126.com>
2013-11-08 12:59:43 +0800

    去除test url，防止误操作，污染数据库

--- 0/login/urls.py
+++ 0/login/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
 	url(r'^(?P<user_id>\d+)/$', views.userpage, name='userpage'),
 	url(r'^sign/$', views.sign, name='sign'),
 	url(r'^changepassword/$', views.changepassword),
-	url(r'^test/$', views.test),
+	# url(r'^test/$', views.test),
 	# url(r'^login/$', views.login, name='login'),
 	# url(r'^requestweightdata/(?P<user_id>\d+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', views.requestweightdata),
 	# url(r'^submit/$', views.submit, name='submit')
