d241e8e37e269bc885051d7e608051d5ad40ac12
gowithqi <gowithqi@126.com>
2013-11-09 19:05:58 +0800

    优化搜索功能，可以根据课号进行搜索了

--- 0/search/views.py
+++ 0/search/views.py
@@ -14,18 +14,24 @@ def autoComplete(request):
 	if request.method != "POST": raise Http404
 
 	content = request.POST['content']
-	courseList = Course.objects.filter(name__startswith=content).order_by("name")
+	tmp = True
+	for k in content: tmp = tmp and (k.isdigit or k.isalpha)
+	if tmp: courseList = Course.objects.filter(number__startswith=content.upper())
+	else: 	courseList = Course.objects.filter(name__startswith=content).order_by("name")
 	res = ''
 	i = 1
-	tmpc = courseList[0]
-	res = res = res + tmpc.name + '\n'
-	for c in courseList[1:]: 
-		if c.name != tmpc.name:	
-			res = res + c.name + '\n'
-			i = i + 1
-		tmpc = c
-		if i == 5: break
-	res = res[:-1]
+	if len(courseList) > 0:
+		tmpc = courseList[0]
+		if tmp: res = res + tmpc.number + ": "
+		res = res = res + tmpc.name + '\n'
+		for c in courseList[1:]: 
+			if c.name != tmpc.name:	
+				if tmp: res = res + c.number + ": "
+				res = res + c.name + '\n'
+				i = i + 1
+			tmpc = c
+			if i == 5: break
+		res = res[:-1]
 
 	return HttpResponse(res)
 
@@ -34,8 +40,10 @@ def searchLecture(request):
 
 	checkUserLogin(request)
 	lecture_id = 0
+	keyword = request.POST['content']
 	try:
-		course = Course.objects.get(name=request.POST['content'])
+		if isCourseNumber(keyword): course = Course.objects.get(number = keyword[:5].upper())
+		else:						course = Course.objects.get(name   = keyword)
 		lectures = course.lecture_set.all()
 		if len(lectures) > 0: lecture_id = lectures[0].id
 		else : lecture_id = -1
@@ -43,3 +51,8 @@ def searchLecture(request):
 		lecture_id = -1
 	
 	return HttpResponse("/lecture/" + str(lecture_id) + "/")
+
+def isCourseNumber(keyword):
+	res = True
+	for k in keyword[:5]: res = res and (k.isdigit or k.isalpha)
+	return res
