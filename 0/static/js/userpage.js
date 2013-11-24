f1073351e7d7907f1afd111bd209b6ce3d532d41
lin5316 <mneutralr@gmail.com>
2013-11-24 19:43:45 +0800

    change lecture

--- 0/static/js/userpage.js
+++ 0/static/js/userpage.js
@@ -1,12 +1,29 @@
 var clr=0,cur=0,hid=0,len=0,laststr="",origin_val="";
+var stack,fTop=0,tTop=0;
 $(document).ready(function(){
-  var trs1=$("#rank1").children(),
+  /*var trs1=$("#rank1").children(),
       trs2=$("#rank2").children();
   for (var i=0;i<10;i++){
     trs1.eq(i).children().eq(0).html(i+1);
     trs2.eq(i).children().eq(0).html(i+1);
+  }*/
+  $("#cPrevious").prop('disabled',true);
+  stack=new Array();
+  stack[0]=new Array();
+  for (var i=0;i<3;i++){
+    var h3=$("#panel_change").find(".panel-title").eq(i);
+    stack[0][i]=new Object();
+    stack[0][i].id=h3.find("a").attr("href").replace("/","").replace("lecture","");
+    stack[0][i].course_name=h3.find("a").html();
+    stack[0][i].professor_name=h3.find("p").html();
+    var p=$("#panel_change").find(".panel-body").eq(i).find("p");
+    stack[0][i].most_popular_comment=p.eq(0).html();
+    stack[0][i].user=p.eq(1).html();
   }
+  fTop=1;
+  tTop=1;
   $.csrftoken();
+  $.ajaxSetup({async:false});
 });
 function auto_complete_post(){
   $.post("/search/lecture/autocomplete/",
@@ -105,11 +122,63 @@ function search_get(cid){
   });
 }
 function change_course_get(){
-  $.get("/userpage/change/",function(data){
-    var s=data.split("\n");
+  if (tTop==fTop){
+    stack[fTop]=new Array();
+    $.get("/userpage/change/",function(data){
+      obj=JSON.parse(data);
+      for (var i=0;i<3;i++){
+        var h3=$("#panel_change").find(".panel-title").eq(i);
+        h3.find("a").attr("href","/lecture/"+obj[i].id+"/");
+        h3.find("a").html(obj[i].course_name);
+        h3.find("p").html(obj[i].professor_name);
+        stack[fTop][i]=new Object();
+        var p=$("#panel_change").find(".panel-body").eq(i).find("p"),comment=obj[i].most_popular_comment;
+        if (comment==""){
+          p.eq(0).html("还没有评论。快来抢沙发！");
+          p.eq(1).html("");
+          stack[fTop][i].most_popular_comment="还没有评论。快来抢沙发！";
+          stack[fTop][i].user="";
+        }
+        else{
+          p.eq(0).html(comment.comment_content);
+          p.eq(1).html(comment.comment_user+"       "+comment.time+"   有用 ("+comment.comment_super_number+")");
+          stack[fTop][i].most_popular_comment=comment.comment_content;
+          stack[fTop][i].user=p[1].html;
+        }
+        stack[fTop][i].id=obj[i].id;
+        stack[fTop][i].course_name=obj[i].course_name;
+        stack[fTop][i].professor_name=obj[i].professor_name;
+      }
+    });
+    tTop++;
+    fTop++;
+  }
+  else{
     for (var i=0;i<3;i++){
-
+      var h3=$("#panel_change").find(".panel-title").eq(i);
+      h3.find("a").attr("href","/lecture/"+stack[fTop][i].id+"/");
+      h3.find("a").html(stack[fTop][i].course_name);
+      h3.find("p").html(stack[fTop][i].professor_name);
+      var p=$("#panel_change").find(".panel-body").eq(i).find("p");
+      p.eq(0).html(stack[fTop][i].most_popular_comment);
+      p.eq(1).html(stack[fTop][i].user)
     }
+    fTop++;
+  }
+  $("#cPrevious").find("span").attr("style","");
+  $("#cPrevious").prop('disabled',false);
+}
+function decollect_get(lid){
+  $.get("/userpage/decollect/lecture/"+lid+"/",function(data){
+    if (data=="yes"){
+      window.location.assign("/");
+    }
+  });
+}
+function all_collection_get(){
+  $.get("/userpage/getallcollectionlectures/",function(data){
+    var obj=JSON.parse(data);
+    
   });
 }
 function auto_complete(){
@@ -180,4 +249,22 @@ $("#submit").click(function(e){
 $("#eSearch").keydown(function(e){
   if (e.keyCode==13)
     $(this).modal("hide");
+});
+$("#cPrevious").click(function(){
+  if (fTop!=1){
+    fTop--;
+    for (var i=0;i<3;i++){
+      var h3=$("#panel_change").find(".panel-title").eq(i);
+      h3.find("a").attr("href","/lecture/"+stack[fTop-1][i].id+"/");
+      h3.find("a").html(stack[fTop-1][i].course_name);
+      h3.find("p").html(stack[fTop-1][i].professor_name);
+      var p=$("#panel_change").find(".panel-body").eq(i).find("p");
+      p.eq(0).html(stack[fTop-1][i].most_popular_comment);
+      p.eq(1).html(stack[fTop-1][i].user);
+    }
+    if (fTop==1){
+      $("#cPrevious").prop('disabled',true);
+      $("#cPrevious").find("span").attr("style","color:#DDDDDD;");
+    }
+  }
 });
