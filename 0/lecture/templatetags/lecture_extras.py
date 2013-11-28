68d91f89bee1f517506b27c971baf2528fc28e4d
gowithqi <gowithqi@126.com>
2013-11-28 21:50:03 +0800

    加入时间差显示

--- 0/lecture/templatetags/lecture_extras.py
+++ 0/lecture/templatetags/lecture_extras.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 from django import template
-
+from datetime import datetime, timedelta
 register = template.Library()
 
 @register.filter
@@ -13,3 +14,20 @@ def div(value, key):
 	if key == 0: return 0
 	res = float(value)/key
 	return int(res*100)
+
+@register.filter
+def computeTimeDelta(i_time):
+	t = datetime.now() - i_time
+
+	t = t.total_seconds()   #second
+	if t < 60: return str(int(t)) + "秒"
+	t = t / 60		#minute
+	if t < 60: return str(int(t)) + "分"
+	t = t / 60		#hour
+	if t < 24: return str(int(t)) + "小时"
+	t = t / 24		#day
+	if t < 30: return str(int(t)) + "天"
+	t = t / 30 		#month
+	if t < 12: return str(int(t)) + "月"
+	t = t / 12
+	return str(int(t)) + "年"
