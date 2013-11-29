ac7a560c7eaa4f08f9915c0785656c9975938d05
gowithqi <gowithqi@126.com>
2013-11-29 21:22:50 +0800

    time_delta

--- 0/login/templatetags/login_extras.py
+++ 0/login/templatetags/login_extras.py
@@ -8,14 +8,14 @@ def computeTimeDelta(i_time):
 	t = datetime.now() - i_time
 
 	t = t.total_seconds()   #second
-	if t < 60: return str(int(t)) + "秒"
+	if t < 60: return str(int(t)) + u"秒"
 	t = t / 60		#minute
-	if t < 60: return str(int(t)) + "分"
+	if t < 60: return str(int(t)) + u"分"
 	t = t / 60		#hour
-	if t < 24: return str(int(t)) + "小时"
+	if t < 24: return str(int(t)) + u"小时"
 	t = t / 24		#day
-	if t < 30: return str(int(t)) + "天"
+	if t < 30: return str(int(t)) + u"天"
 	t = t / 30 		#month
-	if t < 12: return str(int(t)) + "月"
+	if t < 12: return str(int(t)) + u"月"
 	t = t / 12
-	return str(int(t)) + "年"
+	return str(int(t)) + u"年"
