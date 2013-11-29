5667f88b0baa05f3a45695651d66bbb80715928d
gowithqi <gowithqi@126.com>
2013-11-29 17:14:25 +0800

    用户主页URL

--- 0/userpage/views.py
+++ 0/userpage/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render, get_object_or_404
@@ -163,6 +164,46 @@ def getComments(request, start, end):
 
 	return HttpResponse(json.dumps(ret, ensure_ascii=False, indent=4))
 
+@require_http_methods(['GET'])
+def getGossips(request, start, end):
+	start = int(start)
+	if start<1: raise Http500	
+	end = int(end)
+	user_id = checkUserLogin(request)
+	user = get_object_or_404(User, id=user_id)
+
+	if end < start: gossips = user.gossip_set.all().order_by('-time')[start-1:]
+	else: gossips = user.gossip_set.all().order_by('-time')[start-1: end]
+
+	ret = []
+	for g in gossips:
+		tmp = getGossipDict(g)
+		l = g.lecture
+		tmp_l = getLectureDict(l)
+		tmp['lecture'] = tmp_l
+		ret.append(tmp)
+	return HttpResponse(json.dumps(ret, ensure_ascii=False, indent=4))
+
+
+@require_http_methods(['GET'])
+def getRandomComment(request):
+	c = LectureComment.objects.order_by('?')[0]
+	tmp = getCommentDict(c)
+	tmp_l = getLectureDict(c.lecture)
+	tmp['lecture'] = tmp_l
+
+	return HttpResponse(json.dumps(tmp, ensure_ascii=False, indent=4))
+
+def getGossipDict(gossip):
+	gossip_tmp = {}
+	gossip_tmp['gossip_id'] = gossip.id
+	gossip_tmp['gossip_user'] = gossip.user.name
+	gossip_tmp['gossip_content'] = gossip.content
+	gossip_tmp['gossip_super_number'] = gossip.super_number
+	gossip_tmp['gossip_time'] = gossip.time.strftime("%Y-%m-%d %H:%M:%S")
+	gossip_tmp['gossip_time_delta'] = computeTimeDelta(gossip.time)
+	return gossip_tmp
+
 def getCommentDict(comment):
 	comment_tmp = {}
 	comment_tmp['comment_id'] = comment.id
@@ -170,6 +211,7 @@ def getCommentDict(comment):
 	comment_tmp['comment_content'] = comment.content
 	comment_tmp['comment_super_number'] = comment.super_number
 	comment_tmp['comment_time'] = comment.time.strftime("%Y-%m-%d %H:%M:%S")
+	comment_tmp['comment_time_delta'] = computeTimeDelta(comment.time)
 	return comment_tmp
 
 def getLectureDict(l):
@@ -180,3 +222,19 @@ def getLectureDict(l):
 	tmp['professor_name'] = l.professor.name
 	tmp['level'] = l.level
 	return tmp
+
+def computeTimeDelta(i_time):
+	t = datetime.now() - i_time
+
+	t = t.total_seconds()   #second
+	if t < 60: return str(int(t)) + u"秒"
+	t = t / 60		#minute
+	if t < 60: return str(int(t)) + u"分"
+	t = t / 60		#hour
+	if t < 24: return str(int(t)) + u"小时"
+	t = t / 24		#day
+	if t < 30: return str(int(t)) + u"天"
+	t = t / 30 		#month
+	if t < 12: return str(int(t)) + u"月"
+	t = t / 12
+	return str(int(t)) + u"年"
