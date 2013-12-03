7724032d1ff43b0ec9a5945f0400dc2156772e34
lin5316 <mneutralr@gmail.com>
2013-12-03 19:20:47 +0800

    昵称名最长20字 修复太长时的显示问题（只在课程主页上出现）

--- 0/static/js/search.js
+++ 0/static/js/search.js
@@ -132,7 +132,7 @@ function checkname(){
       $("#nm_prompt").html("昵称名太短！");
       $("#nm_prompt").attr("class","text-danger control-label");
     }
-    else if ($("#newname").val().length>15){
+    else if ($("#newname").val().length>20){
       $("#nm_prompt").html("昵称名太长！");
       $("#nm_prompt").attr("class","text-danger control-label");
     }
@@ -239,7 +239,7 @@ $("#eSearch").keydown(function(e){
     $(this).modal("hide");
 });
 $("#newname").focus(function(){
-  $("#helpname").html("2-15位中英字符和数字 不区分大小写");
+  $("#helpname").html("2-20位中英字符和数字 不区分大小写");
 })
 $("#newname").keydown(function(e){
   if (e.keyCode==13)
