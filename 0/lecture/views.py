a073c84f29a177d88b36e752558d649430d569aa
gowithqi <gowithqi@126.com>
2014-01-17 19:50:47 +0800

    add tongshike

--- 0/lecture/views.py
+++ 0/lecture/views.py
@@ -194,11 +194,11 @@ def courselist(request):
 	user_id = checkUserLogin(request)
 	user = get_object_or_404(User, id=user_id)
 	template = loader.get_template('lecture/courselist.html')
-	school_list = [c['school'] for c in Course.objects.values('school').distinct()]
+	all_courses = Course.objects.all()
 	print school_list
 	context = RequestContext(request, {
 		'u': user,
-		'school_list': school_list
+		'c': all_courses,
 		})
 	return HttpResponse(template.render(context))
 
